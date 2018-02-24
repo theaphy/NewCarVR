77a3d14cead67756f3e68783dcd04d54d0502094
theaphy <theheaphy@gmail.com>
2018-02-24 12:39:07 -0500

    lez do this

---
+++ Assets/Scripts/LevelUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelUp : MonoBehaviour {
+
+	string namer;
+
+	void Start () {
+		namer = this.name;
+	}
+
+	void OnTriggerEnter (Collider thing) {
+
+		Debug.Log ("Level" + namer.ToString());
+		SceneManager.LoadScene ("Level" + namer.ToString());
+
+	}
+}
